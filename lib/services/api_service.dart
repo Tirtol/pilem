5a7e8ff1cc4bace84ec983bebb79d812e86e2ef5
Tirtol <126873152+Tirtol@users.noreply.github.com>
2024-03-01 19:29:45 +0700

    update

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http; //d4b79a377a964e0e5b846e669a52bf62
 
 class ApiService {
   static const String baseUrl = "https://api.themoviedb.org/3";
-  static const String apiKey = 'd4b79a377a964e0e5b846e669a52bf62';
+  static const String apiKey = '6a8f370aa98b22838995b471741ad0a4';
   Future<List<Map<String, dynamic>>> getAllMovies() async {
     final response =
         await http.get(Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"));
