9dc821e6cba27be83a6742db6e509f920c18c61d
Tirtol <126873152+Tirtol@users.noreply.github.com>
2024-03-01 19:13:18 +0700

    Update Homescreen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -4,69 +4,66 @@ import 'package:pilem/models/movie.dart';
 class DetailScreen extends StatelessWidget {
   final Movie movie;
   const DetailScreen({super.key, required this.movie});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(movie.title),
       ),
-      body:Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network(
-            'https://image.tmdb.org/t/p/w500${movie.backdropPath}',
-            height: 300,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Overview',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 10),
-          Text(movie.overview),
-          const SizedBox(height: 20),
-          Row(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Icon(
-                Icons.calendar_month_outlined,
-                color: Colors.blue[600],
+              Image.network(
+                'https://image.tmdb.org/t/p/w500${movie.backdropPath}',
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
               ),
-              SizedBox(width: 10),
-              Text(
-                'Release Date',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold
-                ),
-              ),
-              SizedBox(width: 10),
-              Text(movie.releaseDate),
               const SizedBox(height: 20),
-          Row(
-            children: [
-              Icon(
-                Icons.star,
-                color: Colors.yellow,
+              const Text(
+                'Overview:',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              SizedBox(width: 10),
-              Text(
-                'Rating',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold
-                ),
+              const SizedBox(height: 10),
+              Text(movie.overview),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.calendar_month,
+                    color: Colors.blue,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    'Release Date:',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(width: 10),
+                  Text(movie.releaseDate),
+                ],
               ),
-              SizedBox(width: 10),
-              Text(movie.voteAverage.toString()),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    'Rating:',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(width: 10),
+                  Text(movie.voteAverage.toString()),
+                ],
+              ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,27 +1,27 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http; //d4b79a377a964e0e5b846e669a52bf62
 
-class ApiServices {
-  static const String baseUrl = 'https://api.themoviedb.org/3';
+class ApiService {
+  static const String baseUrl = "https://api.themoviedb.org/3";
   static const String apiKey = 'd4b79a377a964e0e5b846e669a52bf62';
-
-  Future<List<Map<String, dynamic>>> getaAllMovies() async {
-    final response = await http.get(Uri.parse('$baseUrl/movie/now_playing?api_key=$apiKey'));
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
+    final response =
+        await http.get(Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"));
     final data = json.decode(response.body);
     return List<Map<String, dynamic>>.from(data['results']);
-  }//API buat liat film yg lagi tayang
+  }
 
   Future<List<Map<String, dynamic>>> getTrendingMovies() async {
-    final response =
-        await http.get(Uri.parse('$baseUrl/movie/week?api_key=$apiKey'));
+    final response = await http
+        .get(Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"));
     final data = json.decode(response.body);
     return List<Map<String, dynamic>>.from(data['results']);
-  } //API buat liat film yang lagi trending
+  }
 
   Future<List<Map<String, dynamic>>> getPopularMovies() async {
     final response =
-        await http.get(Uri.parse('$baseUrl/movie/popular?api_key=$apiKey'));
+        await http.get(Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"));
     final data = json.decode(response.body);
     return List<Map<String, dynamic>>.from(data['results']);
-  } //API buat liat film yg populer
+  }
 }
